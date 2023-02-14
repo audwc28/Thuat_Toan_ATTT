1f33dbbcbfa8c623fdae53afbce543eb192d732f
audwc28 <auducis28@gmail.com>
2023-02-14 19:18:34 +0700

    update snt

--- SNT/check.c
+++ SNT/check.c
@@ -1,24 +1,19 @@
 #include <stdio.h>
 
 int main() {
-  long long N;
-  scanf("%lld", &N);
-  int check[N + 1];
-  // Khởi tạo tất cả các số [2...N] đều là số nguyên tố
-  for (long long i = 2; i <= N; i++) {
-    check[i] = 1;
-  }
-
-  // Thuật toán sàng nguyên tố
-  // Nếu một số là số nguyên tố, thì tất cả các bội của nó không phải số nguyên tố
-  for (long long i = 2; i <= N; i++) {
-    if (check[i] == 1) {
-      for (long long j = i * i; j <= N; j += i) {
-        check[j] = 0;
-      }
+  int dem = 0;
+  long long n;
+  scanf("%lld", &n);
+  for (long long i = 1; i*i <= n; i++)
+  {
+    if (n % i == 0)
+    {
+      dem++;
+      long long j = n/i;
+      if(i != j) dem++;
     }
   }
-  if (check[N] == 1) printf("YES");
+  if(dem == 2) printf("YES");
   else printf("NO");
 }
-// 1/10
+// 6/10
